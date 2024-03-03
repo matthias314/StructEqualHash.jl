56f3510b20f35ff598c5cff043ef14c34d31ccd0
matthias314 <matthias314@posteo.net>
2024-03-03 15:15:12 -0500

    added second macro parameter to choose fields

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ It aims to be lightweight and does not use `@generated` code.
 
 The macro call
 ```julia
-@struct_equal_hash T
+@struct_equal_hash T [fields]
 ```
 generates definitions for `==`, `isequal` and `hash` for the struct `T`,
 ```julia
@@ -18,9 +18,11 @@ generates definitions for `==`, `isequal` and `hash` for the struct `T`,
     function hash(x::T, h0::UInt)
 ```
 
-The equality tests are applied, using short-circuit logic, to all fields of `T`
+The equality tests are applied, using short-circuit logic, to the fields of `T`
+specified by the tuple `fields`. The default for `fields` is all fields of `T`
 in the order in which they have been declared. The hash is similarly computed
-over the type name `T` as well as over all fields. Empty structs are allowed.
+over the type name `T` as well as over the fields given by `fields`.
+Empty structs are allowed.
 
 The type can be a `UnionAll` type like `T{P} where P`. In this case the
 definitions are
@@ -55,3 +57,26 @@ If both types may differ, you can say
     @struct_equal_hash T{P where P,Q where Q}
 ```
 or again omit the parameters.
+
+## Examples
+```julia
+julia> struct T{P}; x::Int; y::P end
+
+julia> @struct_equal_hash T
+
+julia> @struct_equal_hash T{Char} (:x,)
+
+julia> @struct_equal_hash T{P} where P <: Number (:y,)
+
+julia> T(1, "a") == T(1, "b")   # method for T
+false
+
+julia> T(1, [1, 2]) == T(1, [1.0, 2.0])   # method for T
+true
+
+julia> T(1, 'a') == T(1, 'b')   # method for T{Char}
+true
+
+julia> T(1, 1) == T(2, 1)       # method for T{P} where P <: Number
+true
+```
--- src/StructEqualHash.jl
+++ src/StructEqualHash.jl
@@ -6,7 +6,7 @@ Base.@assume_effects :foldable typeid(T::Type) = objectid(T)
 
 typehash(::Type{T}, h::UInt = UInt(0)) where T = hash(3*h-typeid(T))
 
-totuple(x::T) where T = ntuple(i -> getfield(x, i), fieldcount(T))
+totuple(x, fields) = map(name -> getfield(x, name), fields)
 
 iswhere(ex) = Meta.isexpr(ex, :where)
 
@@ -21,7 +21,7 @@ function wrap(ex, ex2)
 end
 
 """
-    @struct_equal_hash T
+    @struct_equal_hash T [fields]
 
 Generate definitions for `==`, `isequal` and `hash` for the struct `T`,
 ```julia
@@ -30,9 +30,11 @@ Generate definitions for `==`, `isequal` and `hash` for the struct `T`,
     function hash(x::T, h0::UInt)
 ```
 
-The equality tests are applied, using short-circuit logic, to all fields of `T`
+The equality tests are applied, using short-circuit logic, to the fields of `T`
+specified by the tuple `fields`. The default for `fields` is all fields of `T`
 in the order in which they have been declared. The hash is similarly computed
-over the type name `T` as well as over all fields. Empty structs are allowed.
+over the type name `T` as well as over the fields given by `fields`.
+Empty structs are allowed.
 
 The type can be a `UnionAll` type like `T{P} where P`. In this case the
 definitions are
@@ -67,16 +69,39 @@ If both types may differ, you can say
     @struct_equal_hash T{P where P,Q where Q}
 ```
 or again omit the parameters.
+
+# Examples
+```julia
+julia> struct T{P}; x::Int; y::P end
+
+julia> @struct_equal_hash T
+
+julia> @struct_equal_hash T{Char} (:x,)
+
+julia> @struct_equal_hash T{P} where P <: Number (:y,)
+
+julia> T(1, "a") == T(1, "b")   # method for T
+false
+
+julia> T(1, [1, 2]) == T(1, [1.0, 2.0])   # method for T
+true
+
+julia> T(1, 'a') == T(1, 'b')   # method for T{Char}
+true
+
+julia> T(1, 1) == T(2, 1)       # method for T{P} where P <: Number
+true
+```
 """
-macro struct_equal_hash(TW)
+macro struct_equal_hash(TW, fields = :(fieldnames($(esc(TW)))))
     T = esc(peel(TW))
     ex1 = wrap(TW, :(Base.:(==)(x::$T, y::$T)))
-    ex2 = wrap(TW, :(Base.isequal(x::$T, y::$T)))    
+    ex2 = wrap(TW, :(Base.isequal(x::$T, y::$T)))
     ex3 = wrap(TW, :(Base.hash(x::$T, h::UInt)))
     quote
-        $ex1 = totuple(x) == totuple(y)
-        $ex2 = isequal(totuple(x), totuple(y))
-        $ex3 = hash(totuple(x), typehash($T, h))
+        $ex1 = totuple(x, $fields) == totuple(y, $fields)
+        $ex2 = isequal(totuple(x, $fields), totuple(y, $fields))
+        $ex3 = hash(totuple(x, $fields), typehash($T, h))
     end
 end
 
